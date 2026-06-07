b4a7b987b3c3b36c1ee6d023884193a69c2bde06
sydomori <sydneyomori@gmail.com>
2026-06-07 03:06:31 +0300

    create book class

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self.page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self.page_count = value
+
+
+book1 = Book("And Then There Were None", 272)
+print(book1.page_count)
     
         

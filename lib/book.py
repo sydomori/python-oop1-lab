1e699910a8504919d9236977a914efa596d99868
sydomori <sydneyomori@gmail.com>
2026-06-07 03:50:29 +0300

    fx: fix recursion error

--- lib/book.py
+++ lib/book.py
@@ -7,14 +7,14 @@ class Book:
 
     @property
     def page_count(self):
-        return self.page_count
+        return self._page_count
     
     @page_count.setter
     def page_count(self, value):
         if not isinstance(value, int):
             print("page_count must be an integer")
         else:
-            self.page_count = value
+            self._page_count = value
 
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
